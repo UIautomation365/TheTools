3b36e767876b42609b76134df3edc5e63e98b459
ajay <ajay.toradmal@globant.con>
2019-02-25 20:48:04 +0530

    updated test file

--- src/test1.java
+++ src/test1.java
@@ -1,4 +1,20 @@
 
 public class test1 {
+	public static void main(String args[]) {
+		int a[]= {20,10,20,10,40};
+		int c=0;
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				if(a[i]==a[j]) {
+					c=c+1;
+				}
+				
+			}
+			if(c>=1) {
+				System.out.println("number"+a[i]+" count"+c);
+			}
+			c=0;
+		}
+	}
 
 }
