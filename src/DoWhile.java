06e4bc85a86c5ef613bf138348c0293c2cd53233
ajay <ajay.toradmal@globant.con>
2019-02-26 10:03:38 +0530

    updated dowihile.java

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -4,11 +4,17 @@ public class DoWhile {
 	public static void main(String args[]) {
 		int i=0;
 		do {
+
 			System.out.println(i);
 			System.out.println("i is="+i);
 			System.out.println("my i is ="+i);
 			System.out.println("my i is="+i);
 			System.out.println("my i is"+i);
+
+			System.out.println("i is="+i);
+			System.out.println("i is="+i);
+			System.out.println("i is="+i);
+
 			i++;
 		}
 		while(false);
